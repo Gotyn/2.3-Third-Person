c61b83d18587e3f86d1062112a9146b8b3ad7345
Martijn Snip <martijnsnip@gmail.com>
2016-02-22 11:51:43 +0100

    Added SoundLoading to Audio::Play()
    Separated Music and Audio --> updated the CONFIG accordingly.

--- mge/src/mge/config.hpp
+++ mge/src/mge/config.hpp
@@ -7,6 +7,7 @@ namespace config {
     const std::string MGE_TEXTURE_PATH ("mge/textures/");
     const std::string MGE_SHADER_PATH ("mge/shaders/");
     const std::string MGE_FONT_PATH ("mge/fonts/");
-    const std::string MGE_AUDIO_PATH ("mge/audio/");
+    const std::string MGE_SOUNDS_PATH ("mge/audio/sounds/");
+    const std::string MGE_MUSIC_PATH ("mge/audio/music/");
 }
 #endif // CONFIG_H
--- mge/src/mge/core/Audio.cpp
+++ mge/src/mge/core/Audio.cpp
@@ -1,5 +1,5 @@
 #include "Audio.hpp"
-#include "Utils.hpp"
+#include "mge/util/Utils.hpp"
 
 Audio* Audio::instance = NULL;
 
@@ -11,57 +11,61 @@ Audio* Audio::Instance() {
     return instance;
 }
 
-Audio::Audio(){
-    audioPath = config::MGE_AUDIO_PATH.c_str();
+Audio::Audio() {
+    soundsPath = config::MGE_SOUNDS_PATH.c_str();
+    musicPath = config::MGE_MUSIC_PATH.c_str();
 }
 
-Audio::~Audio(){
+Audio::~Audio() {
 }
 
-void Audio::PreloadAudio()
-{
+void Audio::PreloadAudio() {
     Instance()->LoadSounds();
 }
 
+//Not used for now... But we can keep this in there as an option :)
 void Audio::LoadSounds() {
-
-    std::vector<std::string> soundNames = Utils::findFilesIn(audioPath);
+    std::vector<std::string> soundNames = Utils::findFilesIn(soundsPath);
 
     for (size_t i=0; i < soundNames.size(); i++)
     {
-        AddSound(soundNames[i], soundNames[i]);
+        AddSound(soundNames[i]);
         std::cout << soundNames[i] << " preloaded!" << std::endl;
     }
 }
 
-void Audio::AddSound(std::string soundName, std::string fileName) {
-    Buffers[soundName].loadFromFile(config::MGE_AUDIO_PATH + fileName);
-    if (Buffers[soundName].getSampleCount() == 0)
-        std::cout << "WARNING: You're playing an empty buffer on '" << soundName << "'!" << std::endl;
-    Sounds[soundName].setBuffer(Buffers[soundName]);
+void Audio::AddSound(std::string fileName) {
+    Buffers[fileName].loadFromFile(soundsPath + fileName);
+    if (Buffers[fileName].getSampleCount() == 0)
+        std::cout << "WARNING: You're playing an empty buffer on '" << fileName << "'!" << std::endl;
+    Sounds[fileName].setBuffer(Buffers[fileName]);
 }
 
-sf::Sound &Audio::GetSound(std::string soundName) {
-    Sounds[soundName].setBuffer(Buffers[soundName]);
-    return Sounds[soundName];
+sf::Sound &Audio::GetSound(std::string fileName) {
+    Sounds[fileName].setBuffer(Buffers[fileName]);
+    return Sounds[fileName];
 }
 
-void Audio::Play(std::string soundName)
-{
-    Instance()->GetSound(soundName).play();
+void Audio::Play(std::string fileName) {
+    instance = Instance();
+    // Check if sound is loaded
+    if(instance->Sounds.find(fileName) == Instance()->Sounds.end()) {   // Sound not loaded.
+        instance->AddSound(fileName);                                   // Load sound.
+    }
+    Instance()->GetSound(fileName).play();                              // Play sound.
 }
 
-void Audio::AddMusic(std::string musicName, std::string fileName){
+void Audio::AddMusic(std::string fileName) {
     auto music = std::unique_ptr<sf::Music>(new sf::Music());
-    music->openFromFile(config::MGE_AUDIO_PATH + fileName);
-    MapMusic(musicName,std::move(music));
+    music->openFromFile(musicPath + fileName);
+    MapMusic(fileName,std::move(music));
 }
 
 //musicName is the map key.
-void Audio::MapMusic(std::string musicName, std::unique_ptr<sf::Music> value){
-    Music.insert(std::pair<std::string, std::unique_ptr<sf::Music> >(musicName, std::move(value)));
+void Audio::MapMusic(std::string fileName, std::unique_ptr<sf::Music> value) {
+    Music.insert(std::pair<std::string, std::unique_ptr<sf::Music> >(fileName, std::move(value)));
 }
 
-sf::Music &Audio::GetMusic(std::string musicName){
-    return *Music.at(musicName);
+sf::Music &Audio::GetMusic(std::string fileName) {
+    return *Music.at(fileName);
 }
