9da6d082f2df5bef95fe51a50a757b419297ca0e
jacques jordaan <jordaanza@gmail.com>
2016-03-06 16:25:24 +0100

    depthmap up vector fixed

--- mge/src/mge/materials/DepthMapper.cpp
+++ mge/src/mge/materials/DepthMapper.cpp
@@ -33,12 +33,11 @@ void DepthMapper::render(World* pWorld, GameObject* pGameObject, Mesh* pMesh, Ca
 
     //with the generated depth texture we can attach it as the framebuffer's depth buffer
     glBindFramebuffer(GL_FRAMEBUFFER, _depthMapFBO);
-//    glClear(GL_DEPTH_BUFFER_BIT);
 
     //light space transform
-    GLfloat near_plane = 1.0f, far_plane = 20.0f;
+    GLfloat near_plane = 1.0f, far_plane = 10.0f;
     glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
-    glm::mat4 lightView = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f), glm::vec3(1.0));
+    glm::mat4 lightView = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
     glm::mat4 lightSpaceMatrix = lightProjection * lightView;
 
