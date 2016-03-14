115b71e9862e3803a3c1ce592e0f62ab68f89c71
jacques jordaan <jordaanza@gmail.com>
2016-03-14 20:48:49 +0100

    particles work

--- mge/src/mge/behaviours/ParticleSystem.cpp
+++ mge/src/mge/behaviours/ParticleSystem.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "ParticleSystem.hpp"
 #include "GameObject.hpp"
 
@@ -34,29 +36,120 @@ ParticleSystem::ParticleSystem()
     glBindBuffer(GL_ARRAY_BUFFER, _particles_color_buffer);
     // Initialize with empty (NULL) buffer : it will be updated later, each frame.
     glBufferData(GL_ARRAY_BUFFER, _maxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+
+    for(int i=0; i<_maxParticles; i++)
+    {
+		_particlesContainer[i].life = -1.0f;
+		_particlesContainer[i].cameradistance = -1.0f;
+	}
 }
 
 ParticleSystem::~ParticleSystem()
 {
-    //dtor
+
+}
+
+void ParticleSystem::sortParticles()
+{
+    std::sort(&_particlesContainer[0], &_particlesContainer[_maxParticles]);
 }
 
 void ParticleSystem::update(float step)
 {
-    // Update the buffers that OpenGL uses for rendering.
-    glBindBuffer(GL_ARRAY_BUFFER, _particles_position_buffer);
-    glBufferData(GL_ARRAY_BUFFER, _maxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning
-    // BROKEN
-//    glBufferSubData(GL_ARRAY_BUFFER, 0, _particlesCount * sizeof(GLfloat) * 4, _g_particule_position_size_data);
+    // Generate 10 new particule each millisecond,
+    // but limit this to 16 ms (60 fps), or if you have 1 long frame (1sec),
+    // newparticles will be huge and the next frame even longer.
+    int newparticles = (int)(step*10000.0);
+    if (newparticles > (int)(0.016f*10000.0))
+        newparticles = (int)(0.016f*10000.0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, _particles_color_buffer);
-    glBufferData(GL_ARRAY_BUFFER, _maxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
-    // BROKEN
-//    glBufferSubData(GL_ARRAY_BUFFER, 0, _particlesCount * sizeof(GLubyte) * 4, _g_particule_color_data);
+    for(int i=0; i<newparticles; i++)
+    {
+        int particleIndex = findUnusedParticle();
+        _particlesContainer[particleIndex].life = 5.0f; // This particle will live 5 seconds.
+        _particlesContainer[particleIndex].pos = glm::vec3(0,0,-20.0f);
+
+        float spread = 1.5f;
+        glm::vec3 maindir = glm::vec3(0.0f, 10.0f, 0.0f);
+        // Very bad way to generate a random direction;
+        // See for instance http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution instead,
+        // combined with some user-controlled parameters (main direction, spread, etc)
+        glm::vec3 randomdir = glm::vec3(
+            (rand()%2000 - 1000.0f)/1000.0f,
+            (rand()%2000 - 1000.0f)/1000.0f,
+            (rand()%2000 - 1000.0f)/1000.0f
+        );
+
+        _particlesContainer[particleIndex].speed = maindir + randomdir*spread;
+
+        // Very bad way to generate a random color
+        _particlesContainer[particleIndex].r = rand() % 256;
+        _particlesContainer[particleIndex].g = rand() % 256;
+        _particlesContainer[particleIndex].b = rand() % 256;
+        _particlesContainer[particleIndex].a = (rand() % 256) / 3;
+
+        _particlesContainer[particleIndex].size = (rand()%1000)/2000.0f + 0.1f;
+    }
+
+    // Simulate all particles
+    int particlesCount = 0;
+    for(int i=0; i<_maxParticles; i++)
+    {
+//        std::cout << i << std::endl;
+
+        Particle& p = _particlesContainer[i]; // shortcut
+
+//        if(p.life > 0.0f)
+//        {
+//            // Decrease life
+//            p.life -= step;
+//            if (p.life > 0.0f)
+//            {
+//                // Simulate simple physics : gravity only, no collisions
+//                p.speed += glm::vec3(0.0f,-9.81f, 0.0f) * (float)step * 0.5f;
+//                p.pos += p.speed * (float)step;
+////                p.cameradistance = glm::length2( p.pos - CameraPosition );
+//                //ParticlesContainer[i].pos += glm::vec3(0.0f,10.0f, 0.0f) * (float)delta;
+//
+//                // Fill the GPU buffer
+//                _g_particule_position_size_data[4*_particlesCount+0] = p.pos.x;
+//                _g_particule_position_size_data[4*_particlesCount+1] = p.pos.y;
+//                _g_particule_position_size_data[4*_particlesCount+2] = p.pos.z;
+//
+//                _g_particule_position_size_data[4*_particlesCount+3] = p.size;
+//
+//                _g_particule_color_data[4*_particlesCount+0] = p.r;
+//                _g_particule_color_data[4*_particlesCount+1] = p.g;
+//                _g_particule_color_data[4*_particlesCount+2] = p.b;
+//                _g_particule_color_data[4*_particlesCount+3] = p.a;
+//            }
+//            else
+//            {
+//                // Particles that just died will be put at the end of the buffer in SortParticles();
+//                p.cameradistance = -1.0f;
+//            }
+//
+//            _particlesCount++;
+//        }
+    }
+
+    sortParticles();
 }
 
 void ParticleSystem::render()
 {
+    // Update the buffers that OpenGL uses for rendering.
+    glBindBuffer(GL_ARRAY_BUFFER, _particles_position_buffer);
+    glBufferData(GL_ARRAY_BUFFER, _maxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning
+    glBufferSubData(GL_ARRAY_BUFFER, 0, _particlesCount * sizeof(GLfloat) * 4, _g_particule_position_size_data);
+
+    glBindBuffer(GL_ARRAY_BUFFER, _particles_color_buffer);
+    glBufferData(GL_ARRAY_BUFFER, _maxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, _particlesCount * sizeof(GLubyte) * 4, _g_particule_color_data);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     // 1st attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, _billboard_vertex_buffer);
--- mge/src/mge/behaviours/ParticleSystem.hpp
+++ mge/src/mge/behaviours/ParticleSystem.hpp
@@ -11,6 +11,13 @@ struct Particle
     unsigned char r,g,b,a; // Color
     float size, angle, weight;
     float life; // Remaining life of the particle. if < 0 : dead and unused.
+    float cameradistance; // *Squared* distance to the camera. if dead : -1.0f
+
+    bool operator<(const Particle& that) const
+    {
+		// Sort in reverse order : far particles drawn first.
+		return this->cameradistance > that.cameradistance;
+	}
 };
 
 class ParticleSystem : public AbstractBehaviour
@@ -26,12 +33,15 @@ class ParticleSystem : public AbstractBehaviour
     protected:
 
     private:
+        void sortParticles();
         int findUnusedParticle();
         // The VBO containing the 4 vertices of the particles.
         // Thanks to instancing, they will be shared by all particles.
         static GLfloat _g_vertex_buffer_data[12];
-
         static const int _maxParticles = 1000;
+
+        GLfloat* _g_particule_position_size_data = new GLfloat[_maxParticles * 4];
+        GLubyte* _g_particule_color_data = new GLubyte[_maxParticles * 4];
         int _particlesCount;
         int _lastUsedParticle = 0;
         Particle _particlesContainer[_maxParticles];
