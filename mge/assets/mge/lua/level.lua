192d88726a38aa7275b06a4ba146915a5b78aa9f
Chiggy\Chiggy <casey.lloyd.mgid@gmail.com>
2016-03-16 16:35:14 +0100

    small change
    small change

--- mge/assets/mge/lua/level.lua
+++ mge/assets/mge/lua/level.lua
@@ -33,12 +33,12 @@ storyWall:scale(0.7, 0.7, 0.7)
 storyWall:setPosition (3.1, 2.3, 3.3)
 
 -- PRE-CACHING STORY-WALL IMAGES STARTS HERE
-function cacheWallTextures()
+--[[function cacheWallTextures()
     for i, v in ipairs(story) do 
         storyWall:changeTexture(story[i].wallImage)
     end
 end
-cacheWallTextures()
+cacheWallTextures()]]--
 -- PRE-CACHING STORY-WALL IMAGES ENDS HERE
 
 spotlight = Game.getSpotlight()
