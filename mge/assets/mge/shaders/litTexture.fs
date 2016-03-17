baf379fba38c37be294dfb31e17c1ed04d2e13ed
jacques jordaan <jordaanza@gmail.com>
2016-03-17 12:21:19 +0100

    Revert "fail"
    This reverts commit afcd0dff4084086debdf0506ac2653dfd2126def.

--- mge/assets/mge/shaders/litTexture.fs
+++ mge/assets/mge/shaders/litTexture.fs
@@ -4,7 +4,6 @@ out vec4 FragColor;
 in VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
-<<<<<<< HEAD
     vec3 TangentLightPos;
     vec3 TangentViewPos;
     vec3 TangentFragPos;
@@ -12,17 +11,9 @@ in VS_OUT {
 
 uniform sampler2D diffuseMap;
 uniform sampler2D normalMap;
-=======
-    vec4 FragPosLightSpace;
-} fs_in;
-
-uniform sampler2D diffuseTexture;
-uniform sampler2D shadowMap;
->>>>>>> parent of a7e6ec8... close to working
 
 void main()
 {
-<<<<<<< HEAD
     // Obtain normal from normal map in range [0,1]
     vec3 normal = texture(normalMap, fs_in.TexCoords).rgb;
     // Transform normal vector to range [-1,1]
@@ -30,15 +21,6 @@ void main()
 
     // Get diffuse color
     vec3 color = texture(diffuseMap, fs_in.TexCoords).rgb;
-=======
-    vec3 lightDir = normalize(light.position - fs_in.FragPos);
-
-//    // Check if lighting is inside the spotlight cone
-//    float theta = dot(lightDir, normalize(-light.direction));
-
-    vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb * diffuseColor;
-    vec3 normal = normalize(fs_in.Normal);
->>>>>>> parent of a7e6ec8... close to working
     // Ambient
     vec3 ambient = 0.1 * color;
     // Diffuse
